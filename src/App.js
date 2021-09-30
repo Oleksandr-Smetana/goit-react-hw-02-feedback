1851f962a0772e53400d428c3b40ba99256184eb
Oleksandr-Smetana <anatemso@gmail.com>
2021-09-30 14:06:01 +0300

    добавляет сырой вариант разметки

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Section from './components/Title/Title';
+// import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // onBtnGoodClick = () => {
+  //   this.setState({
+  //     good: this.state.good + 1,
+  //   });
+  // };
+
+  onBtnGoodClick = () => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  onBtnNeutralClick = () => {
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  };
+
+  onBtnBadClick = () => {
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+  };
+
+  getTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  getGoodFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.getTotalFeedback()) * 100);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">Please leave feedback</h1>
+
+        {/* <FeedbackOptions
+          options={this.state}
+          onLeaveFeedback={
+            (this.onBtnGoodClick, this.onBtnNeutralClick, this.onBtnBadClick)
+          }
+        /> */}
+        <div className="buttons">
+          <button type="button" className="good" onClick={this.onBtnGoodClick}>
+            Good
+          </button>
+          <button
+            type="button"
+            className="neutral"
+            onClick={this.onBtnNeutralClick}
+          >
+            Neutral
+          </button>
+          <button type="button" className="bad" onClick={this.onBtnBadClick}>
+            Bad
+          </button>
+        </div>
+        <div className="statWrap">
+          <h1 className="statTitle">Statistics</h1>
+          {this.getTotalFeedback() ? (
+            <ul>
+              <li>Good: {this.state.good}</li>
+              <li>Neutral: {this.state.neutral}</li>
+              <li>Bad: {this.state.bad}</li>
+              <li>Total: {this.getTotalFeedback()}</li>
+              <li>Positive feedback: {this.getGoodFeedbackPercentage()}%</li>
+            </ul>
+          ) : (
+            <p>No feedback given</p>
+          )}
+        </div>
+
+        {/* <Section title="" />
+        <FeedbackOptions options={ } onLeaveFeedback={ } />
+      <Statistics good={ } neutral={ } bad={ } total={ } positivePercentage={ } />
+      <Notification message="No feedback given" /> */}
+      </div>
+    );
+  }
 }
-
-export default App;
+export default Feedback;
---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+
+// export default function FeedbackOptions({options, onLeaveFeedback }) {
+//     return (
+//         <div className="buttons">
+//             <button
+//                 type="button"
+//                 className="good"
+//                 onClick={}>
+//             Good
+//           </button>
+//           <button
+//             type="button"
+//             className="neutral"
+//             onClick={}
+//           >
+//             Neutral
+//           </button>
+//             <button
+//                 type="button"
+//                 className="bad"
+//                 onClick={}>
+//             Bad
+//           </button>
+//         </div>
+//     )
+// }
